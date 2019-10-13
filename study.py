5c2fde30d72039953c4b94be4f226e65cb183de5
Ryan Blachowicz <ryansblachowicz@gmail.com>
2019-10-13 10:50:11 -0400

    can write to cards.txt

--- study.py
+++ study.py
@@ -3,6 +3,7 @@
 
 import random
 import os
+import sys
 
 # get cards
 front = []
@@ -16,11 +17,48 @@ while line:
     line = fr.readline()
 fr.close()
 
-operation = input("1. Edit flashcards\n2. Study ")
+os.system("clear")
+operation = input("1. Edit flashcards\n2. Study \n")
 
 # edit cards
+i = 0
+os.system("clear")
 while operation == "1":
-    print("edit")
+    # print items
+    print("ITEMS")
+    while i < len(front):
+        print(str(i) + ":", front[i])
+        print("  ", back[i])
+        i += 1
+    operation = input("\n\nOPTIONS\n1. Clear all\n2. Add items\n")
+
+    # clear items
+    if operation == "1":
+        open("./cards.txt", "w").close()
+        front = []
+        back = []
+        os.system("clear")
+
+    # add items
+    while operation == "2":
+        os.system("clear")
+        # print items
+        print("ITEMS")
+        i = 0
+        while i < len(front):
+            print(str(i) + ":", front[i])
+            print("  ", back[i])
+            i += 1
+        fa = open("./cards.txt", "a")
+        write = input("\nEnter question text.\nType 'done' to exit. \n")
+        if write == "done":
+            fa.close()
+            sys.exit()
+        elif write != "done":
+            fa.write(write)
+            os.system("clear")
+        write = input("\nEnter the answer text of that question.\n")
+        fa.write("\n" + write + "\n") 
 
 # study cards
 while operation == "2":
